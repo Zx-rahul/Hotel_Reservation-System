6270b02e8244546a68c0022e61b33f4605244353
Zx-rahul <bwbsrahul@gmail.com>
2024-08-12 09:36:07 +0530

    Created different classes to origanise

---
+++ src/Main_Menu.java
@@ -0,0 +1,79 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.Scanner;
+
+
+public class Main_Menu {//jdbc:mysql://127.0.0.1:3306/?user=root
+    private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
+    private static final String username = "root";
+    private static final String password = "root";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            Connection connection = DriverManager.getConnection(url, username, password);
+            while(true){
+                System.out.println();
+                System.out.println("======================⟪⟪ HOTEL MANAGEMENT " +
+                        "SYSTEM ⟫⟫======================\n");
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("1. Reserve a room");
+                System.out.println("2. View Reservations");
+                System.out.println("3. Get Room Number");
+                System.out.println("4. Update Reservations");
+                System.out.println("5. Delete Reservations");
+                System.out.println("0. Exit");
+                System.out.print("Choose an option: ");
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        ReserveRoom.reserveRoom(connection, scanner);
+                        break;
+                    case 2:
+                        ViewReservation.viewReservations(connection);
+                        break;
+                    case 3:
+                        GetRoomNumber.getRoomNumber(connection, scanner);
+                        break;
+                    case 4:
+                        UpdateReservation.updateReservation(connection, scanner);
+                        break;
+                    case 5:
+                        DeleteReservation.deleteReservation(connection, scanner);
+                        break;
+                    case 0:
+                        exit();
+                        scanner.close();
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Try again.");
+                }
+            }
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public static void exit() throws InterruptedException {
+        System.out.print("Exiting System");
+        int i = 5;
+        while(i!=0){
+            System.out.print(".");
+            Thread.sleep(1000);
+            i--;
+        }
+        System.out.println();
+        System.out.println("ThankYou For Using Hotel Reservation System!!!");
+    }
+}
